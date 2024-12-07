b6137f46e3db85511beff839b8eca5e52452c882
Gabriel Santos <ebert.gabrielst@gmail.com>
2024-12-07 01:17:39 -0300

    Update

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,11 @@
-import type { Config } from "drizzle-kit";
-import { env } from "@/env"
+import type { Config } from 'drizzle-kit'
+import { env } from '@/env'
 
 export default {
-  schema: "./src/db/schema/index.ts",
-  out: "./drizzle",
-  dialect: "postgresql",
+  schema: './src/db/schema/index.ts',
+  out: './drizzle',
+  dialect: 'postgresql',
   dbCredentials: {
     url: env.DB_URL,
-  }
-} satisfies Config;
+  },
+} satisfies Config
